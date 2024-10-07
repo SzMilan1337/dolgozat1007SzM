adefaec48573aa98789530d3a7fd2db115274fe0
szalatnai.milan <szalatnai.milan@28-DIAK7>
2024-10-07 15:06:26 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using dolgozat1007SzM;
+using System.Text;
+
+
+List<versenyzo> versenyzok = [];
+using StreamReader sr = new(
+    path: @"..\..\..\src\forras.txt",
+    encoding: Encoding.UTF8);
+_ = sr.ReadLine();
+while (!sr.EndOfStream) versenyzok.Add(new(sr.ReadLine()));
+
+Console.WriteLine($"4. Feladat: {versenyzok.Count} versenyző fejezte be a versenyt. ");
+int korkateg = versenyzok.Count(p => p.kategoria == "elit");
+Console.WriteLine($"5. Feladat: {korkateg} db versenyző van a(z) '25-29' kategóriában.");
+var atlageletkor = versenyzok.Where(v => v.nem);
+double atlelet = (atlageletkor.Count()*DateTime.Now.Year - atlageletkor.Sum( v => v.szul_ev )) / (double)atlageletkor.Count();
+Console.WriteLine($"6. Feladat: Az átlag életkora a versenyzőknek: {atlelet}.");
+double uszashsz = Math.Round(versenyzok.Sum(v => v.idok["uszas"].TotalHours), 2);
+Console.WriteLine($"7. Feladat: A versenyzők úszással töltött ideje, órákban: {uszashsz}.");
---
+++ Verseny.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace dolgozat1007SzM
+{
+    internal class versenyzo
+    {
+        public string nev { get; set; }
+        public int szul_ev { get; set; }
+        public int rajtszam { get; set; }
+        public bool nem { get; set; }
+
+        public string kategoria { get; set; }
+
+        public Dictionary<string, TimeSpan> idok = new Dictionary<string, TimeSpan>();
+
+        public versenyzo(string sor)
+        {
+            var v = sor.Split(";");
+            nev = v[0];
+            szul_ev = Convert.ToInt32(v[1]);
+            rajtszam = Convert.ToInt32(v[2]);
+            nem = v[3] == "n";
+            kategoria = v[4];
+            idok.Add("uszas", TimeSpan.Parse(v[5]));
+            idok.Add("elsodepo", TimeSpan.Parse(v[6]));
+            idok.Add("kerekpar", TimeSpan.Parse(v[7]));
+            idok.Add("masodikdepo", TimeSpan.Parse(v[8]));
+            idok.Add("futas", TimeSpan.Parse(v[9]));
+
+        }
+
+        public override string ToString()
+        {
+            return $"Versenyző neve: {nev}" +
+                $"\tNeme: {(nem ? "Nő" : "Férfi")}" +
+                $"\tRajtszáma: {rajtszam}," +
+                $"\tSzületési éve: {szul_ev} " +
+                $"\nIdejei: " +
+                $"\n\tÚszás: {idok["uszas"]} " +
+                $"\n\tElső depo: {idok["elsodepo"]}" +
+                $"\n\tKerékpár: {idok["kerekpar"]}" +
+                $"\n\tMásodik depo: {idok["masodikdepo"]}" +
+                $"\n\tFutás: {idok["futas"]}";
+        }
+    }
+}
